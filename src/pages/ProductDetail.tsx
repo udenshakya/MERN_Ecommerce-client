0160b3687fcfa3a1b8b04f2863675621a7b37489
uden shakya <udenshakya98@gmail.com>
2024-02-07 21:25:32 +0545

    Cloudinary fix

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -11,7 +11,7 @@ const ProductDetail = () => {
 
   const { data } = useProductsDetailsQuery(params.id!);
 
-  const photo = data?.product.image.url!;
+  const image = data?.product?.image!;
   const name = data?.product.name!;
   const price = data?.product.price!;
   const stock = data?.product.stock!;
@@ -29,7 +29,7 @@ const ProductDetail = () => {
     <div>
       <main className="sm:grid h-[80vh] place-items-center p-3 bg-gray-100 container mx-auto grid-cols-2 gap-6  mt-10">
         <div className="h-[300px] w-[400px]">
-          <img src={photo} className="rounded-lg w-full h-full object-contain" />
+          <img src={image?.url} className="rounded-lg w-full h-full object-contain" />
         </div>
 
         <div className="flex flex-col gap-6 justify-center">
@@ -42,7 +42,7 @@ const ProductDetail = () => {
             onClick={() =>
               addToCartHandler({
                 productId,
-                photo,
+                image,
                 price,
                 name,
                 stock,
